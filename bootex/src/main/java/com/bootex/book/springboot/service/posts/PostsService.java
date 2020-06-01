b017dbc3736f8b32b003d2e091a656c5c4569387
dgkim <dgkim@123.111.253.60>
2020-06-02 00:43:42 +0900

    12

--- bootex/src/main/java/com/bootex/book/springboot/service/posts/PostsService.java
+++ bootex/src/main/java/com/bootex/book/springboot/service/posts/PostsService.java
@@ -1,5 +1,20 @@
 package com.bootex.book.springboot.service.posts;
 
-public class PostsService {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.bootex.book.springboot.domain.posts.PostsRepository;
+import com.bootex.book.springboot.web.dto.PostsSaveRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+	private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
--- bootex/src/main/java/com/bootex/book/springboot/web/PostsApiController.java
+++ bootex/src/main/java/com/bootex/book/springboot/web/PostsApiController.java
@@ -1,5 +1,21 @@
 package com.bootex.book.springboot.web;
 
-public class PostsApiController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.bootex.book.springboot.service.posts.PostsService;
+import com.bootex.book.springboot.web.dto.PostsSaveRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+	private final PostsService postsService;
+
+	@PostMapping("/api/v1/posts")
+	public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+		return postsService.save(requestDto);
+	}
 }
