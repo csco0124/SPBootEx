7460e21e88abdfc4ef83564a4bc28624c1a5410b
dgkim <dgkim@123.111.253.51>
2020-06-03 01:58:17 +0900

    12

--- bootex/src/main/java/com/bootex/book/springboot/web/IndexController.java
+++ bootex/src/main/java/com/bootex/book/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.bootex.book.springboot.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.bootex.book.springboot.config.auth.dto.SessionUser;
 import com.bootex.book.springboot.service.posts.PostsService;
 import com.bootex.book.springboot.web.dto.PostsResponseDto;
 
@@ -14,10 +17,15 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class IndexController {
 	private final PostsService postsService;
+	private final HttpSession httpSession;
 	
 	@GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+        	model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 	
