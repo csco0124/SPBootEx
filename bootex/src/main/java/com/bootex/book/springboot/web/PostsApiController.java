40edffb261169cdcedd182faaacc3dd80e29ce0a
dgkim <dgkim@123.111.253.51>
2020-06-03 00:48:43 +0900

    12

--- bootex/src/main/java/com/bootex/book/springboot/web/PostsApiController.java
+++ bootex/src/main/java/com/bootex/book/springboot/web/PostsApiController.java
@@ -1,5 +1,8 @@
 package com.bootex.book.springboot.web;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bootex.book.springboot.service.posts.PostsService;
+import com.bootex.book.springboot.web.dto.PostsListResponseDto;
 import com.bootex.book.springboot.web.dto.PostsResponseDto;
 import com.bootex.book.springboot.web.dto.PostsSaveRequestDto;
 import com.bootex.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -19,18 +23,29 @@ import lombok.RequiredArgsConstructor;
 public class PostsApiController {
 	private final PostsService postsService;
 
-	@PostMapping("/api/v1/posts")
-	public Long save(@RequestBody PostsSaveRequestDto requestDto) {
-		return postsService.save(requestDto);
-	}
-	
-	@PutMapping("/api/v1/posts/{id}")
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
-	
-	@GetMapping("/api/v1/posts/{id}")
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
